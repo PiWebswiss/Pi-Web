b412a28c58f3fe8cf2f66d42b8ab6702e8707c58
PiWeb <81098558+PiWebswiss@users.noreply.github.com>
2022-08-29 12:32:16 +0200

    first commit

---
+++ PiWebaffutage/static/script.js
@@ -0,0 +1,47 @@
+let url = window.location.href + "";
+if(url.indexOf("http://") === 0) window.location.href = url.replace("http://", "https://");
+
+/* constant */
+
+
+window.addEventListener('scroll', function(e) {
+  
+  target = document.querySelectorAll('.scroll');
+  
+  index =  0, length = target.length;
+
+  for (index; index < length; index++) {
+      rate = window.pageYOffset * target[index].dataset.rate;
+      target[index].style.transform = 'translate3d(0px,'+rate+'px, 0px)';
+  }
+});
+ 
+function navMobile() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    let navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-black" ;
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-black", "");
+    }
+}
+
+
+
+/* footer */
+const d = new Date();
+const footer = document.getElementById("footerPage");
+const a = gt("a", "PIWeb", { href: "PIWeb", className: "w3-hover-text-blue"});
+const span = gt("span", "© " + d.getFullYear() + " Copyright : Powered by ");
+const p = gt("p", [span, a]);
+/* footer.innerHTML = "<p>© " + d.getFullYear() + " Copyright : Powered by" + `<a class="w3-hover-text-blue" href="PIWeb">PIWeb</a></p>` */
+footer.appendChild(p);
---
+++ script.js
@@ -0,0 +1,58 @@
+let url = window.location.href + "";
+if(url.indexOf("http://") === 0) window.location.href = url.replace("http://", "https://");
+
+const gt = (tag, children, attributes) => {
+  const element = document.createElement(tag);
+  for (const key in attributes) {
+      element[key] = attributes[key];
+  }
+  if (children) {
+      if (typeof children === "string") element.innerText = children;
+      else if (Array.isArray(children)){
+          for (let i = 0; i < children.length; i++) element.appendChild(children[i]);
+      }else element.appendChild(children);
+  }
+  return element;
+};
+
+
+const text = document.getElementById("textAnimation");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for(let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++ 
+  if(char === splitText.length){
+    complete();
+    return;
+  }
+}
+
+function complete(){
+  clearInterval(timer);
+  timer = null;
+}
+
+/* mail */
+const btnMail = document.getElementById("btnMail");
+btnMail.addEventListener("click", function() {location.href = "mailto:piweb%40gmail.com"});
+
+/* to remove !!!!! */
+console.log("to remove !", encodeURIComponent("piweb@gmail.com"))
+
+/* footer */
+const d = new Date();
+const footer = document.getElementById("footerPage");
+const a = gt("a", "PIWeb", { href: "#"});
+const span = gt("span", "© " + d.getFullYear() + " Copyright : Powered by ", { className: "no-transition white"});
+const p = gt("p", [span, a])
+footer.appendChild(p);
